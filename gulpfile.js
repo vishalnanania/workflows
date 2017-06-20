913b18ac6a1d775a0866fa2e8e93855f027fc7f3
Vishal Nananiya <vishal.nanania@gmail.com>
2017-06-20 11:13:16 -0400

    added gulp concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffeeSource = ['components/coffee/*.coffee'];
+var jsSource = ['components/scripts/*.js'];
 
 gulp.task('log', function(){
     gutil.log('workflows are awesome');
@@ -13,4 +15,11 @@ gulp.task('coffee', function(){
         .pipe(coffee({bare: true}))
             .on('error', gutil.log)
         .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function(){
+    gulp.src(jsSource)
+        .pipe(concat('script.js'))
+            .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/js'))
 });
