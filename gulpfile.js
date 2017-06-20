8c4ed1cb07d15cf033ef7e7f4dd057a0170dfbf6
Vishal Nananiya <vishal.nanania@gmail.com>
2017-06-19 22:37:39 -0400

    added goffee script files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
+
+var coffeeSource = ['components/coffee/*.coffee'];
 
 gulp.task('log', function(){
     gutil.log('workflows are awesome');
+});
+
+gulp.task('coffee', function(){
+    gulp.src(coffeeSource)
+        .pipe(coffee({bare: true}))
+            .on('error', gutil.log)
+        .pipe(gulp.dest('components/scripts'))
 });
